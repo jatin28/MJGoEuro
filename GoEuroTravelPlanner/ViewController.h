258147ac3f5ff72e8d91a9bca0d946cff9ed9802
Jatin Mohindra <jatinmohindra@Jatins-MacBook-Pro-2.local>
2016-10-24 11:22:24 +0530

    First Commit

---
+++ GoEuroTravelPlanner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GoEuroTravelPlanner
+//
+//  Created by Jatin Mohindra on 08/10/16.
+//  Copyright © 2016 Jatin Mohindra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GoEuroTravelPlanner/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GoEuroTravelPlanner
+//
+//  Created by Jatin Mohindra on 08/10/16.
+//  Copyright © 2016 Jatin Mohindra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
